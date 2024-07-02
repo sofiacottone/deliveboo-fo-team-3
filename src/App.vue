f39ea4e56da780268385c6018b90877142fe7cc3
sofiacottone <cottonesofia@outlook.it>
2024-07-02 13:08:15 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        Hello World!
+    </div>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+@use './style/partials/variables' as *;
+</style>
