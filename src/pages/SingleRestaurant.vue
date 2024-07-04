0c1968f46be68d5cee7b77c77feeb0a20d4bc49e
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-07-04 16:54:46 +0200

    feat: fix dish card modal (to fix)

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { store } from '../store.js';
 import ShoppingCart from '../components/ShoppingCart.vue';
 
-
 export default {
     name: 'SingleRestaurant',
     components: {
@@ -13,6 +12,7 @@ export default {
         return {
             store,
             restaurant: {},
+            selectedDish: null,
         }
     },
     methods: {
@@ -23,23 +23,23 @@ export default {
                     console.log(this.restaurant);
                 });
         },
-
         getImageUrl(name) {
             return new URL(`../assets/img/${name}`, import.meta.url).href;
         },
         hasHistory() {
             return window.history.length > 2
+        },
+        selectDish(dish) {
+            this.selectedDish = dish;
         }
     },
-
-
     mounted() {
         this.getSingleRestaurant();
         console.log(this.store.apiBaseUrl + '/storage');
     },
 }
-
 </script>
+
 
 <template>
     <div class="container py-3">
@@ -54,7 +54,7 @@ export default {
                 <h2>{{ restaurant.restaurant_name }}</h2>
                 <p>{{ restaurant.description }}</p>
                 <i class="fa-solid fa-location-dot"></i> <span> {{ restaurant.address }}</span>
-                <div v-for="category in restaurant.categories">
+                <div v-for="category in restaurant.categories" :key="category.id">
                     <span class="badge mx-1">{{ category.name }}</span>
                 </div>
             </div>
@@ -64,8 +64,12 @@ export default {
                 <div class="col">
                     <h2>Piatti</h2>
                     <div class="hstack gap-3 flex-wrap">
-                        <div class="col-3 border rounded h-100 pb-2" data-bs-toggle="modal"
-                            data-bs-target="#staticBackdrop " data-dish-name="{{dish.name}}" v-for=" dish in restaurant.dishes ">
+                        <div class="col-3 border rounded h-100 pb-2" 
+                             v-for="dish in restaurant.dishes"
+                             :key="dish.id"
+                             data-bs-toggle="modal"
+                             data-bs-target="#staticBackdrop"
+                             @click="selectDish(dish)">
                             <img :src="dish.image ? `${this.store.apiBaseUrl}/storage/${dish.image}` : getImageUrl('fast-food.webp')"
                                 class="card-img-top rounded-top ms-dish-img" :alt="dish.name">
                             <div class="py-2 px-2 fw-bold">{{ dish.name }}</div>
@@ -78,7 +82,6 @@ export default {
                                     <i class="fa-solid fa-plus p-1"></i>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -92,20 +95,27 @@ export default {
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ selectedDish ? selectedDish.name : '' }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <div class="card-img-top rounded-top ">
+                        <img :src="selectedDish && selectedDish.image ? `${store.apiBaseUrl}/storage/${selectedDish.image}` : getImageUrl('fast-food.webp')" :alt="selectedDish ? selectedDish.name : ''" class="ms-img-style ms-dish-img">
+
+                    </div>
+                    <p v-if="selectedDish">{{ selectedDish.description }}</p>
+                    <h3 v-if="selectedDish">{{ selectedDish.price }} €</h3>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Understood</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Chiudi</button>
+                    <button type="button" class="btn btn-primary">Aggiungi al carrello</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+
 
 <style lang="scss" scoped>
 @use '../style/generic';
@@ -116,6 +126,9 @@ export default {
     max-width: 100%;
     object-fit: cover;
     object-position: center;
+        .ms-img-style{
+            height: 200px;
+    }
 }
 
 .ms-primary {
@@ -125,4 +138,6 @@ export default {
 .badge {
     background-color: $primary-color;
 }
+
+
 </style>
