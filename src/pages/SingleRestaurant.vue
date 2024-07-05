372a8006a4ff7e0494f8bd9a8ce18bfc22601917
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-07-05 11:30:09 +0200

    feat: fix modal

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -15,7 +15,7 @@ export default {
             restaurant: {},
             cart: [],
             selectedDish: null,
-            
+
         }
     },
     methods: {
@@ -23,7 +23,7 @@ export default {
             axios.get(`${this.store.apiBaseUrl}/api/restaurants/${this.$route.params.slug}`)
                 .then((response) => {
                     this.restaurant = response.data.results;
-                    console.log(this.restaurant);
+                    // console.log(this.restaurant);
                 });
         },
 
@@ -34,14 +34,14 @@ export default {
             return window.history.length > 2
         },
         addDishOnCart(dish) {
-  const cartItem = {
-    id: dish.id,
-    name: dish.name,
-    price: dish.price,
-    quantity: 1
-  };
+            const cartItem = {
+                id: dish.id,
+                name: dish.name,
+                price: dish.price,
+                quantity: 1
+            };
 
-  this.cart.push(cartItem);
+            this.cart.push(cartItem);
         },
         selectDish(dish) {
             this.selectedDish = dish;
@@ -56,7 +56,7 @@ export default {
 
     mounted() {
         this.getSingleRestaurant();
-        console.log(this.store.apiBaseUrl + '/storage');
+        // console.log(this.store.apiBaseUrl + '/storage');
     },
 
 
@@ -87,43 +87,59 @@ export default {
                 <div class="col">
                     <h2>Piatti</h2>
                     <div class="hstack gap-3 flex-wrap">
-                        <div class="col-3 border rounded h-100 pb-2"  v-for=" dish in restaurant.dishes ">
-                            <img :src="dish.image ? `${this.store.apiBaseUrl}/storage/${dish.image}` : getImageUrl('fast-food.webp')"
-                                class="card-img-top rounded-top ms-dish-img" :alt="dish.name">
-                            <div class="py-2 px-2 fw-bold" data-bs-toggle="modal"
-                            data-bs-target="#staticBackdrop " data-dish-name="{{dish.name}}">{{ dish.name }}</div>
-                            <div class="py-2 px-2 fw-bold">{{ dish.price }} €</div>
-                            <div class="d-flex justify-content-center align-items-center gap-2 px-2">
-                                <div class="border rounded w-75 text-center ms-primary" role="button">
-                                    <i class="fa-solid fa-trash p-1"></i>
+                        <div class="col-3 border rounded h-100" v-for="dish in restaurant.dishes" :key="dish.id"
+                            @click="selectDish(dish)">
+                            <div class="border rounded h-100 pb-2">
+                                <!-- div bersaglio modale  -->
+                                <div data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                    <img :src="dish.image ? `${this.store.apiBaseUrl}/storage/${dish.image}` : getImageUrl('fast-food.webp')"
+                                        class="card-img-top rounded-top ms-dish-img" :alt="dish.name">
+                                    <div class="py-2 px-2 fw-bold" data-dish-name="{{dish.name}}">{{ dish.name }}</div>
+                                    <div class="py-2 px-2 fw-bold">{{ dish.price }} €</div>
                                 </div>
-                                <div class="border rounded w-75 text-center ms-primary" role="button" @click="addDishOnCart(dish)">
-                                    <i class="fa-solid fa-plus p-1"></i>
-                                </div>
-                            </div>
 
+
+                                <div class="d-flex justify-content-center align-items-center gap-2 px-2">
+                                    <div class="border rounded w-75 text-center ms-primary" role="button">
+                                        <i class="fa-solid fa-trash p-1"></i>
+                                    </div>
+                                    <div class="border rounded w-75 text-center ms-primary" role="button"
+                                        @click="addDishOnCart(dish)">
+                                        <i class="fa-solid fa-plus p-1"></i>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
                 <ShoppingCart :cart="cart" :dishes="restaurant.dishes"></ShoppingCart>
             </div>
         </div>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Understood</button>
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ selectedDish ? selectedDish.name : ''
+                            }}
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card-img-top rounded-top">
+                            <img :src="selectedDish && selectedDish.image ? `${store.apiBaseUrl}/storage/${selectedDish.image}` : getImageUrl('fast-food.webp')"
+                                :alt="selectedDish ? selectedDish.name : ''" class="ms-img-style ms-dish-img">
+
+                        </div>
+                        <p v-if="selectedDish">{{ selectedDish.description }}</p>
+                        <h3 v-if="selectedDish">{{ selectedDish.price }} €</h3>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Chiudi</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" >Aggiungi al carrello</button>
+                    </div>
                 </div>
             </div>
         </div>
