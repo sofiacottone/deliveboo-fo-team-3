9a327ad73177eadd7e49c7b9d439ebb32ac19e14
Alfredodaniello <alfredo_daniello@libero.it>
2024-07-08 10:07:12 +0200

    remove cart page from header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,10 +17,6 @@ export default {
                     label: 'Ristoranti',
                     routeName: 'restaurants'
                 },
-                {
-                    label: 'Carrello',
-                    routeName: 'checkout'
-                }
             ]
         };
     }
@@ -34,22 +30,26 @@ export default {
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
-                    <img src="../assets/img/logo-deliveboo-fo.png" alt="Logo" width="100%" height="34" class="d-inline-block align-text-top">
+                    <img src="../assets/img/logo-deliveboo-fo.png" alt="Logo" width="100%" height="34"
+                        class="d-inline-block align-text-top">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse align-items-baseline justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item" v-for="link in menuLinks">
-                            <router-link class="nav-link" activeClass="ms-active" aria-current="page" :to="{ name: link.routeName }">{{ link.label }}</router-link>
+                            <router-link class="nav-link" activeClass="ms-active" aria-current="page"
+                                :to="{ name: link.routeName }">{{ link.label }}</router-link>
                         </li>
-                        
+
                     </ul>
                     <span class="ps-3">
-                        <a href="http://127.0.0.1:8000/" target="_blank"><button class="ms-btn-custom">Registrati o accedi</button></a>
+                        <a href="http://127.0.0.1:8000/" target="_blank"><button class="ms-btn-custom">Registrati o
+                                accedi</button></a>
                     </span>
-                    
+
                 </div>
             </div>
         </nav>
@@ -58,27 +58,26 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../style/generic';
+@use '../style/partials/variables' as *;
 
-    @use '../style/generic';
-    @use '../style/partials/variables' as *;
+.navbar {
+    background-color: transparent !important;
 
-    .navbar {
-        background-color: transparent !important;
+}
 
+.nav-item>.nav-link {
+    font-family: $secondary-font;
+    font-weight: 500;
+    font-size: 16px;
+    text-transform: uppercase;
+
+    &.ms-active {
+        color: $primary-color !important;
     }
+}
 
-    .nav-item > .nav-link {
-        font-family: $secondary-font;
-        font-weight: 500;
-        font-size: 16px;
-        text-transform: uppercase;
-
-        &.ms-active {
-            color: $primary-color !important;
-        }
-    }
-
-    .ms-btn-custom {
+.ms-btn-custom {
     background-color: #fff;
     font-family: $secondary-font;
     text-transform: uppercase;
@@ -96,5 +95,4 @@ export default {
         transform: scale(1.1);
     }
 }
-
 </style>
