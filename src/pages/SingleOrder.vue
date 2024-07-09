66b1d4ef2a672d519dc28416a2b2b9fcd162018e
DanieleBegnis <daniele.begnis@gmail.com>
2024-07-09 12:40:34 +0200

    add view checkout overview

---
+++ src/pages/SingleOrder.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'SingleOrder',
+
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+
+    }
+
+}
+
+</script>
+<template>
+    <h3>Ecco il recap del tuo ordine</h3>
+    
+</template>
