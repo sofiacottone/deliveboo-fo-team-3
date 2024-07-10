6b65e2793de8c348effa4f03102e95fa0780a442
Alfredodaniello <alfredo_daniello@libero.it>
2024-07-10 12:53:25 +0200

    some changes

--- src/pages/AppPayments.vue
+++ src/pages/AppPayments.vue
@@ -1,4 +1,5 @@
 <script>
+import { Axios } from 'axios';
 import { store } from '../store.js';
 
 export default {
@@ -24,10 +25,7 @@ export default {
                 authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
                 selector: '#dropin-container'
             }, (err, instance) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
+
 
                 button.addEventListener('click', () => {
                     instance.requestPaymentMethod((err, payload) => {
@@ -50,6 +48,36 @@ export default {
                 });
             });
         }
+    },
+    getValidation() {
+        const button = document.querySelector('#submit-button');
+
+        braintree.dropin.create({
+            authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+            selector: '#dropin-container'
+        }, function (err, instance) {
+            button.addEventListener('click', function () {
+                instance.requestPaymentMethod(function (err, payload) {
+                    // Submit payload.nonce to your server
+                });
+                axios.post(`${store.apiBaseUrl}/api/orders`, store.orderData)
+                    .then((response) => {
+                        this.success = response.data.success;
+                        if (this.success) {
+                            console.log(store.orderData);
+                            this.isValid = true;
+                            // this.$router.push({ name: 'order-confirm' });
+                            store.currentRestaurant = null;
+                            store.cart = [];
+                            store.totalPrice = 0;
+                            localStorage.clear();
+                        } else {
+                            this.errors = response.data.errors;
+                        }
+                    })
+            })
+        });
+        this.goToConfirm();
     },
     mounted() {
         this.getValidation();
@@ -75,12 +103,12 @@ export default {
                     <h2>Dettagli dell'Ordine</h2>
                     <ul>
                         <li v-for="(dish, index) in orderData.cart" :key="index">
-                            {{ dish.quantity }}x {{ dish.name }} - {{ dish.price.replace("." , ",") }} €
+                            {{ dish.quantity }}x {{ dish.name }} - {{ dish.price.replace(".", ",") }} €
                         </li>
                     </ul>
                 </div>
 
-                <p><strong>Totale: {{ orderData.totalPrice.toFixed(2).replace("." , ",") }} €</strong></p>
+                <p><strong>Totale: {{ orderData.totalPrice.toFixed(2).replace(".", ",") }} €</strong></p>
                 <div v-for="link in menuLinks">
                     <router-link class="nav-link" activeClass="ms-active" aria-current="page"
                         :to="{ name: link.routeName }">Torna alla Home</router-link>
@@ -134,3 +162,117 @@ ul {
     padding: 0;
 }
 </style>
+
+
+<!-- <script>
+import axios from 'axios';
+import { store } from '../store.js';
+
+export default {
+    name: 'AppHome',
+    data() {
+        return {
+            store,
+            success: null,
+            errors: {},
+            isValid: false
+        }
+    }, methods: {
+        getValidation() {
+            const button = document.querySelector('#submit-button');
+
+            braintree.dropin.create({
+                authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+                selector: '#dropin-container'
+            }, function (err, instance) {
+                button.addEventListener('click', function () {
+                    instance.requestPaymentMethod(function (err, payload) {
+                        // Submit payload.nonce to your server
+                    });
+                    axios.post(`${store.apiBaseUrl}/api/orders`, store.orderData)
+                        .then((response) => {
+                            this.success = response.data.success;
+                            if (this.success) {
+                                console.log(store.orderData);
+                                this.isValid = true;
+                                // this.$router.push({ name: 'order-confirm' });
+                                store.currentRestaurant = null;
+                                store.cart = [];
+                                store.totalPrice = 0;
+                                localStorage.clear();
+                            } else {
+                                this.errors = response.data.errors;
+                            }
+                        })
+                })
+            });
+            this.goToConfirm();
+        },
+        goToConfirm() {
+            if (this.isValid) {
+                this.$router.push({ name: 'order-confirm' });
+            }
+        },
+    },
+    mounted() {
+        this.getValidation()
+    }
+}
+</script>
+
+
+<template>
+    <div v-if="errors">
+        <div v-for="error in errors">
+            {{ error }}
+        </div>
+    </div>
+
+    <div class="container">
+        <h4>Inserisci i dati della carta per effettuare il pagamento</h4>
+        <div class="row">
+            <div class="col-md-8">
+                <div id="dropin-container"></div>
+                <button id="submit-button" class="button button--small button--green">Effettua il
+                    pagamento</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.button {
+    cursor: pointer;
+    font-weight: 500;
+    left: 3px;
+    line-height: inherit;
+    position: relative;
+    text-decoration: none;
+    text-align: center;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 3px;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    display: inline-block;
+}
+
+.button--small {
+    padding: 10px 20px;
+    font-size: 0.875rem;
+}
+
+.button--green {
+    outline: none;
+    background-color: #64d18a;
+    border-color: #64d18a;
+    color: white;
+    transition: all 200ms ease;
+}
+
+.button--green:hover {
+    background-color: #8bdda8;
+    color: white;
+}
+</style> -->
