fadbcee0dbd8cd969ffbbd3ca69b9e3345f13042
sofiacottone <cottonesofia@outlook.it>
2024-07-10 12:27:56 +0200

    feat: add back btn

--- src/pages/AppCheckout.vue
+++ src/pages/AppCheckout.vue
@@ -52,7 +52,10 @@ export default {
                 price: store.totalPrice,
                 dishes: store.cart
             }
-        }
+        },
+        hasHistory() {
+            return window.history.length > 2
+        },
     },
     mounted() {
         // this.sendForm();
@@ -61,90 +64,90 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="container mt-4">
-            <h3>Inserisci i tuoi dati per procedere con il pagamento</h3>
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="card-body">
-                                <form @submit.prevent="handleSubmit(); sendForm()">
-                                    <div class="mb-4 row">
-                                        <label for="fullname" class="col-md-4 col-form-label text-md-right">Nome e
-                                            Cognome<span style="color: red;">*</span>
-                                        </label>
+    <div class="container mt-4">
+        <div class="pb-3 ms-primary" @click="hasHistory() ? $router.go(-1) : $router.push('/')" role="button">
+            <i class="fa-solid fa-arrow-left"></i>
+            Indietro
+        </div>
+        <h3>Inserisci i tuoi dati per procedere con il pagamento</h3>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-body">
+                            <form @submit.prevent="handleSubmit(); sendForm()">
+                                <div class="mb-4 row">
+                                    <label for="fullname" class="col-md-4 col-form-label text-md-right">Nome e
+                                        Cognome<span style="color: red;">*</span>
+                                    </label>
 
-                                        <div class="col-md-6">
-                                            <input type="text" v-model="userFullName" class="form-control" id="fullname"
-                                                pattern=".{5,250}" title="Il nome deve contenere almeno 5 lettere"
-                                                required>
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input type="text" v-model="userFullName" class="form-control" id="fullname"
+                                            pattern=".{5,250}" title="Il nome deve contenere almeno 5 lettere" required>
                                     </div>
-                                    <div class="mb-4 row">
-                                        <label for="email" class="col-md-4 col-form-label text-md-right">Email<span
-                                                style="color: red;">*</span>
-                                        </label>
+                                </div>
+                                <div class="mb-4 row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">Email<span
+                                            style="color: red;">*</span>
+                                    </label>
 
-                                        <div class="col-md-6">
-                                            <input type="email" v-model="userEmail" class="form-control" id="email"
-                                                required>
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input type="email" v-model="userEmail" class="form-control" id="email"
+                                            required>
                                     </div>
-                                    <div class="mb-4 row">
-                                        <label for="address"
-                                            class="col-md-4 col-form-label text-md-right">Indirizzo<span
-                                                style="color: red;">*</span>
-                                        </label>
+                                </div>
+                                <div class="mb-4 row">
+                                    <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo<span
+                                            style="color: red;">*</span>
+                                    </label>
 
-                                        <div class="col-md-6">
-                                            <input type="text" v-model="userAddress" class="form-control" id="address"
-                                                required>
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input type="text" v-model="userAddress" class="form-control" id="address"
+                                            required>
                                     </div>
-                                    <div class="mb-4 row">
-                                        <label for="phone" class="col-md-4 col-form-label text-md-right">Numero di
-                                            telefono<span style="color: red;">*</span>
-                                        </label>
+                                </div>
+                                <div class="mb-4 row">
+                                    <label for="phone" class="col-md-4 col-form-label text-md-right">Numero di
+                                        telefono<span style="color: red;">*</span>
+                                    </label>
 
-                                        <div class="col-md-6">
-                                            <input type="tel" v-model="userPhone" class="form-control" id="phone"
-                                                required pattern="[0-9]{10,13}"
-                                                title="Il numero deve contenere 10 o 12 cifre(con prefisso)">
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input type="tel" v-model="userPhone" class="form-control" id="phone" required
+                                            pattern="[0-9]{10,13}"
+                                            title="Il numero deve contenere 10 o 12 cifre(con prefisso)">
                                     </div>
+                                </div>
 
-                                    <div class="d-flex justify-content-center p-3 pb-0">
-                                        <button type="submit" class="btn btn-primary">Vai al pagamento</button>
-                                    </div>
+                                <div class="d-flex justify-content-center p-3 pb-0">
+                                    <button type="submit" class="btn btn-primary">Vai al pagamento</button>
+                                </div>
 
 
 
-                                </form>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                <div class="w-25 border rounded-1 p-3 ms-cart" v-if="store.cart.length > 0">
-                    <div class="d-flex justify-content-between mb-3">
-                        <div class="fw-bold">Il tuo ordine da {{ store.cart[0].restaurant.restaurant_name }}</div>
+            </div>
+            <div class="w-25 border rounded-1 p-3 ms-cart" v-if="store.cart.length > 0">
+                <div class="d-flex justify-content-between mb-3">
+                    <div class="fw-bold">Il tuo ordine da {{ store.cart[0].restaurant.restaurant_name }}</div>
+                </div>
+                <div class="border rounded-1 p-2">
+                    <div class="d-flex justify-content-between border-bottom px-2 py-4"
+                        v-for="(dish, index) in store.cart">
+                        <div class="d-flex gap-3">
+                            <div>{{ dish.quantity }}x</div>
+                            <div>{{ dish.name }}</div>
+                        </div>
+                        <div class="d-flex gap-3">
+                            <div v-if="dish.quantity == 1">{{ dish.price }} €</div>
+                            <div v-else>{{ store.newPriceArray[dish.id] }} €</div>
+                        </div>
                     </div>
-                    <div class="border rounded-1 p-2">
-                        <div class="d-flex justify-content-between border-bottom px-2 py-4"
-                            v-for="(dish, index) in store.cart">
-                            <div class="d-flex gap-3">
-                                <div>{{ dish.quantity }}x</div>
-                                <div>{{ dish.name }}</div>
-                            </div>
-                            <div class="d-flex gap-3">
-                                <div v-if="dish.quantity == 1">{{ dish.price }} €</div>
-                                <div v-else>{{ store.newPriceArray[dish.id] }} €</div>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-between p-2 mt-2">
-                            <div>Totale dell'ordine</div>
-                            <div class="fw-bold">{{ store.totalPrice.toFixed(2) }} €</div>
-                        </div>
+                    <div class="d-flex justify-content-between p-2 mt-2">
+                        <div>Totale dell'ordine</div>
+                        <div class="fw-bold">{{ store.totalPrice.toFixed(2) }} €</div>
                     </div>
                 </div>
             </div>
@@ -154,4 +157,11 @@ export default {
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../style/generic';
+@use '../style/partials/variables' as *;
+
+.ms-primary {
+    color: $primary-color;
+}
+</style>
