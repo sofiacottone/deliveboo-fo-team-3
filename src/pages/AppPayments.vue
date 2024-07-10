ae1bf9f510d44aedaddb23a98a7361572a10f7a5
Alfredodaniello <alfredo_daniello@libero.it>
2024-07-10 14:55:27 +0200

    change order confirm

--- src/pages/AppPayments.vue
+++ src/pages/AppPayments.vue
@@ -112,21 +112,28 @@ export default {
             </div>
         </template>
         <template v-else>
-            <div class="card p-4">
-                <h1>Conferma dell'Ordine</h1>
-                <div>
-                    <h2>Dettagli dell'Ordine</h2>
-                    <ul>
-                        <li v-for="(dish, index) in orderData.cart" :key="index">
-                            {{ dish.quantity }}x {{ dish.name }} - {{ dish.price.replace(".", ",") }} €
-                        </li>
-                    </ul>
-                </div>
+            <div class="container">
+                <div class="row">
+                    <h3 class="text-center text-success">Ordine effettuato con successo!</h3>
+                    <div class="border rounded p-3">
+                        <div class="h3 border-bottom pb-1">Riepilogo dell'ordine</div>
+                        <div class="h5 mt-3">I tuoi dati:</div>
+                        <div><span class="fw-bold">Indirizzo:</span> {{ store.orderData.address }}</div>
+                        <div><span class="fw-bold">Telefono:</span>{{ store.orderData.phone_number }}</div>
+                        <div><span class="fw-bold">Email:</span>{{ store.orderData.email }}</div>
 
-                <p><strong>Totale: {{ orderData.totalPrice.toFixed(2).replace(".", ",") }} €</strong></p>
-                <div v-for="link in menuLinks">
-                    <router-link class="nav-link" activeClass="ms-active" aria-current="page"
-                        :to="{ name: link.routeName }">Torna alla Home</router-link>
+                        <div class="h5 mt-3">I piatti che hai ordinato</div>
+                        <div v-for="dish in store.orderData.dishes" class="d-flex gap-3">
+                            <div>{{ dish.quantity }}x</div>
+                            <div>{{ dish.name }}</div>
+                            <div>{{ dish.price }} €</div>
+                        </div>
+
+                        <div class="mt-3"><span class="h5">Totale:</span> {{ store.orderData.price }} €</div>
+                        <div></div>
+                    </div>
+                    <h3 class="text-center mt-4">Riceverai il tuo ordine il prima possibile!</h3>
+
                 </div>
             </div>
 
